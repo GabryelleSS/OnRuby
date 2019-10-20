4cac289a42bb1b1dd6ff7551015f2ed8811ce696
Gabryelle SS <gabryelle-gaby@hotmail.com>
2019-10-20 19:46:25 -0300

    Stylig Customer Registration Screen

---
+++ src/br/senac/onRuby/aplicacao/aplicacao.java
@@ -0,0 +1,23 @@
+package br.senac.onRuby.aplicacao;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class aplicacao extends Application {
+    public void start(Stage stage) throws Exception {
+        Parent customerRegistration = FXMLLoader.load(
+            getClass().getResource(
+                "/br/senac/onRuby/view/CustomerRegistration.fxml"
+            )
+        );
+        
+        Scene scene = new Scene(customerRegistration);
+        scene.getStylesheets().add("CustomerRegistration.css");
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/br/senac/onRuby/view/CustomerRegistrationController.java
@@ -0,0 +1,8 @@
+package br.senac.onRuby.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CustomerRegistrationController {
+    
+}
