77d2c155fd39742b31add35b5a65ce3729c18110
Gabryelle SS <gabryelle-gaby@hotmail.com>
2019-11-04 00:29:09 -0300

    Feat: Create data clothes

---
+++ src/br/senac/onRuby/ChooseClothingSale/RegisterSaleClothes/DataRegisterClothes.java
@@ -0,0 +1,61 @@
+package br.senac.onRuby.ChooseClothingSale.RegisterSaleClothes;
+
+import java.time.LocalDate;
+
+public class DataRegisterClothes {
+    
+    private Integer idClothes;
+    private String nameClothes;
+    private int quantityClothes;
+    private float unitPrice;
+    private String sizeClothes;   
+    private String nameProvider;
+    
+    public Integer getIdClothes() {
+        return idClothes;
+    }
+
+    public void setIdClothes(Integer idClothes) {
+        this.idClothes = idClothes;
+    }
+    
+    public String getNameClothes() {
+        return nameClothes;
+    }
+
+    public void setNameClothes(String nameClothes) {
+        this.nameClothes = nameClothes;
+    }
+    
+    public int getQuantityClothes() {
+        return quantityClothes;
+    }
+
+    public void setQuantityClothes(int quantityClothes) {
+        this.quantityClothes = quantityClothes;
+    }
+    
+    public float getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(float unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+    
+    public String getSizeClothes() {
+        return sizeClothes;
+    }
+
+    public void setUnitPrice(String sizeClothes) {
+        this.sizeClothes = sizeClothes;
+    }
+    
+    public String getNameProvider() {
+        return nameProvider;
+    }
+
+    public void setNameProvider(String nameProvider) {
+        this.nameProvider = nameProvider;
+    }
+}
